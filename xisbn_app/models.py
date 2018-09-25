950254fc20faa77e960b4403b33549cb7f8423b5
birkin <birkin.diana@gmail.com>
2018-09-25 15:27:20 -0400

    adds processing status.

--- xisbn_app/models.py
+++ xisbn_app/models.py
@@ -12,10 +12,11 @@ log = logging.getLogger(__name__)
 
 class XisbnTracker( models.Model ):
     canonical_isbn = models.CharField( max_length=13, null=True, blank=True )
-    canonical_isbn_count = models.IntegerField( null=True, blank=True, default=0 )
+    canonical_isbn_count = models.IntegerField( default=0, null=True, blank=True )
     alternates = models.TextField( null=True, blank=True )
     filtered_alternates = models.TextField( null=True, blank=True )
     brown_filtered_alternates = models.TextField( null=True, blank=True )
     bfa_last_changed_date = models.DateTimeField()
+    processing_status = models.CharField( max_length=20, default='not_yet_processed', null=True, blank=True )  # or 'in_process' or 'processd'
     dt_created = models.DateTimeField( auto_now_add=True )
     dt_modified = models.DateTimeField( auto_now=True )
