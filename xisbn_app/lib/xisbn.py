b4fe06e16ac28019a98c85a5a1cac714da598dee
birkin <birkin.diana@gmail.com>
2018-09-25 13:47:48 -0400

    experiments with cache.

--- xisbn_app/lib/xisbn.py
+++ xisbn_app/lib/xisbn.py
@@ -3,6 +3,8 @@
 import datetime, json, logging, os, pprint
 import isbnlib
 from django.core.cache import cache
+from django.http import HttpResponse
+# from xisbn_app import settings_app
 
 
 log = logging.getLogger(__name__)
@@ -15,6 +17,7 @@ class XHelper( object ):
         log.debug( 'initializing helper' )
         # self.legit_services = [ 'isbn', 'oclc' ]
         self.canonical_isbn = ''
+        self.cached_status = ''
 
     def check_isbn_validity( self, isbn ):
         """ Returns boolean.
@@ -28,18 +31,39 @@ class XHelper( object ):
         log.debug( 'validity, `%s`' % validity )
         return validity
 
-
     def get_alternates( self ):
         """ Returns list of alternates.
             Called by views.alternates() and views.filtered_alternates() """
         cache_key = self.canonical_isbn
         alternates = cache.get( cache_key )
         if alternates is None:
-            log.debug( 'alternates were not in cache' )
+            self.cached_status = 'alternates_not_from_cache'
             alternates = isbnlib.editions( self.canonical_isbn, service='merge' )
-            cache.set( cache_key, alternates )  # time-in-seconds could be last argument; defaults to settings.py entry
+            cache.set( cache_key, alternates, (60*60*24*365) )  # time-in-seconds is optional last argument; defaults to settings.py entry
         else:
-            log.debug( 'alternates were in cache' )
-        log.debug( 'alternates, ```%s```' % pprint.pformat(alternates) )
+            self.cached_status = 'alternates_from_cache'
+        log.debug( 'cached_status, `%s`' % self.cached_status )
+        log.debug( 'alternates, ```%s```' % alternates )
         return alternates
 
+
+    def make_alternates_response( self, request, alternates, start_time ):
+        """ Builds unfiltered response.
+            Called by views.alternates() """
+        cntxt = {
+            'request': {
+                'url': '%s://%s%s' % ( request.scheme,
+                    request.META.get( 'HTTP_HOST', '127.0.0.1' ),  # HTTP_HOST doesn't exist for client-tests
+                    request.META.get('REQUEST_URI', request.META['PATH_INFO'])
+                    ),
+                'timestamp': str( start_time )
+            },
+            'response': {
+                'alternates': alternates,
+                'canonical_isbn': self.canonical_isbn,
+                'cached_status': self.cached_status,
+                'elapsed_time': str( datetime.datetime.now()-start_time )
+            }
+        }
+        output = json.dumps( cntxt, sort_keys=True, indent=2 )
+        return HttpResponse( output, content_type='application/json; charset=utf-8' )
