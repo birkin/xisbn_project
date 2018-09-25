bef50d60d2995e08b197fffe3c4e2728ade2cfba
birkin <birkin.diana@gmail.com>
2018-09-25 13:48:56 -0400

    starts modelling.

--- xisbn_app/models.py
+++ xisbn_app/models.py
@@ -6,4 +6,16 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.http import HttpResponseRedirect
 
+
 log = logging.getLogger(__name__)
+
+
+class XisbnTracker( models.Model ):
+    canonical_isbn = models.CharField( max_length=13, null=True, blank=True )
+    canonical_isbn_count = models.IntegerField( null=True, blank=True, default=0 )
+    alternates = models.TextField( null=True, blank=True )
+    filtered_alternates = models.TextField( null=True, blank=True )
+    brown_filtered_alternates = models.TextField( null=True, blank=True )
+    bfa_last_changed_date = models.DateTimeField()
+    dt_created = models.DateTimeField( auto_now_add=True )
+    dt_modified = models.DateTimeField( auto_now=True )
